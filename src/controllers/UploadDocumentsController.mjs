bdae90fa4c77637e96a9368a1e959d1b1a1343b7
Julian Jimenez <julianjimenez2128@gmail.com>
2024-07-29 09:55:55 -0500

    fix(UploadDocumentsController.mjs): Update file reception and storage in database

--- src/controllers/UploadDocumentsController.mjs
+++ src/controllers/UploadDocumentsController.mjs
@@ -1,28 +1,31 @@
 import db from "../DataBase/ConectDb.mjs";
+import multer from "multer";
+
+const upload = multer();
 
 const manageDocuments = {};
 
-manageDocuments.uploadDocumentsController = (req, res, db) => {
+manageDocuments.uploadDocumentsController = async (req, res) => {
   const column = req.body.column;
-  const rowId = req.body.idUser; 
+  const idUser = req.body.idUser;
+  const file = req.file ? req.file.buffer : null;
+
+  console.log(req.body.column);
+  console.log(req.body.idUser);
+  console.log(req.file ? req.file.buffer : "No file buffer");
+
+  if (!file) {
+    return res.status(400).json({ message: "No files were uploaded." });
+  }
 
   try {
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).json({ message: "No files were uploaded." });
-    }
-
-    //Combine all files into a single buffer
-    const combinedBuffer = Buffer.concat(req.files.map((file) => file.buffer));
-
-   
     const query = `UPDATE employeesDocumentation SET ${column} = ? WHERE employeeDocumentationId = ?`;
 
-    db.query(query, [combinedBuffer, rowId], (error, results) => {
+    db.query(query, [file, idUser], (error, results) => {
+      console.log("Archivo guardado");
       if (error) {
         console.error("Error:", error);
-        return res
-          .status(500)
-          .json({ message: "Error uploading files", error });
+        return res.status(500).json({ message: "Error uploading files", error });
       } else {
         res.status(200).json({ message: "Files uploaded successfully" });
       }
@@ -32,7 +35,5 @@ manageDocuments.uploadDocumentsController = (req, res, db) => {
     res.status(500).json({ message: "Error uploading files", error });
   }
 };
-
-
 
 export default manageDocuments;
