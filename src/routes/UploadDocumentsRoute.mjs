8f89ba178fab7b56e32dd3eee869d542ba52f152
Julian Jimenez <julianjimenez2128@gmail.com>
2024-07-29 10:08:35 -0500

    feat(routes): Add new route file to connect with UploadDocumentsController

--- src/routes/UploadDocumentsRoute.mjs
+++ src/routes/UploadDocumentsRoute.mjs
@@ -1,22 +1,11 @@
 import express from 'express';
 import manageDocuments from '../controllers/UploadDocumentsController.mjs';
-import multer from 'multer';
 import db from '../DataBase/ConectDb.mjs';
-
-
-
-
-//Configure multer for file upload
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+import multer from "multer"
 
 const routerUploadDocuments = express.Router();
-
-routerUploadDocuments.post('/upload', upload.array('fileUpload[]'), (req, res) => manageDocuments.uploadDocumentsController(req, res, db));
-
-
-
-
+const upload = multer();
+routerUploadDocuments.post('/upload', upload.single('document'), manageDocuments.uploadDocumentsController);
 
 
 
